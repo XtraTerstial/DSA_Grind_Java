760f4f68c42436cdc7d653147c09760b50498ec2
Devbrat Pradhan <devbratpradhan40@gmail.com>
2026-01-17 23:36:29 +0530

    .

---
+++ src/DelloiteNLA/FindFactorial.java
@@ -0,0 +1,17 @@
+package DelloiteNLA;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class FindFactorial {
+    static void main() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        BigInteger fact = BigInteger.ONE;
+        for (int i = n; i > 0 ; i--) {
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println("Factorial of "+n+" is = "+fact);
+    }
+}
---
+++ src/DelloiteNLA/PrimeOrNot.java
@@ -0,0 +1,34 @@
+package DelloiteNLA;
+
+import java.util.Scanner;
+//Prime check: O(√n)
+//Square root: O(1)
+//Overall: O(√n) → optimal for single number input.
+
+//Check if a number is prime or not and if prime print it's sq root to 2 decimal place
+public class PrimeOrNot {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        if (a <= 1) {
+            System.out.println("Not a prime number");
+            return;
+        }
+
+        boolean isPrime = true;
+        int sqrt = (int)Math.sqrt(a);
+        for (int i = 2; i <= sqrt; i++) {
+            if(a%i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            System.out.println(a+" is a prime number");
+            System.out.printf("%.2f", Math.sqrt(a));
+        } else{
+            System.out.println("Not a prime Number");
+        }
+    }
+}
